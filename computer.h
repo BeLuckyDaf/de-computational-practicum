958014b853bf767a0002a8e402e5a38ebf39ce0a
BeLuckyDaf <beluckydaf@gmail.com>
2018-11-05 02:50:33 +0300

    1.0

--- computer.h
+++ computer.h
@@ -28,7 +28,6 @@ public:
     Computer();
 
     qreal C1 = 1;
-    qreal bound = qPow(std::numeric_limits<qreal>::max(), 0.25);
 
     struct CompleteSeries ComputeEuler(qreal from, qreal to, qreal step, qreal y0);
     struct CompleteSeries ComputeImprovedEuler(qreal from, qreal to, qreal step, qreal y0);
